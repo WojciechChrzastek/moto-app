bd1d4456463669ef9f0c568795684619b8026f71
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-26 18:53:11 +0200

    Add addUser method

--- src/main/java/net/chrzastek/UserService.java
+++ src/main/java/net/chrzastek/UserService.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,11 @@ public class UserService {
   public ResponseEntity getUsers() throws JsonProcessingException {
     List<User> users = userRepository.findAll();
     return ResponseEntity.ok(objectMapper.writeValueAsString(users));
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity addUser(@RequestBody User user) {
+    User savedUser = userRepository.save(user);
+    return ResponseEntity.ok(savedUser);
   }
 }
