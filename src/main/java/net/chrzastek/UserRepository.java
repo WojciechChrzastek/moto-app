bfe3a53fd4b2cabe14bf7013525c30c7b95eab5f
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-26 18:54:59 +0200

    Add findByUsername method to the interface

--- src/main/java/net/chrzastek/UserRepository.java
+++ src/main/java/net/chrzastek/UserRepository.java
@@ -2,5 +2,8 @@ package net.chrzastek;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+  List<User> findByUsername(String username);
 }
