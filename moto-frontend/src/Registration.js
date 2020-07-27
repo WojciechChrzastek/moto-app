d017abc4b973de86bbe3eb5816ec1a690b1fadee
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-27 14:54:31 +0200

    Handle submit register button with registerUser method

--- moto-frontend/src/Registration.js
+++ moto-frontend/src/Registration.js
@@ -5,12 +5,40 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 class Registration extends Component {
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.registerUser(event.target.username.value, event.target.password.value)
+  }
+
+  registerUser(username, password) {
+    fetch('http://localhost:8080/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    }).then(function(response) {
+      if (response.status === 200) {
+        console.log("User registered!");
+      } else {
+        console.log("User not registered");
+      }
+    }).catch(function(error) {
+      console.log("error!");
+    });
+  }
+
   render() {
     return <div className = "Register">
-      <Form>
+      <Form onSubmit = {this.handleSubmit}>
         <Form.Group controlId = "username" size = "lg">
           <Form.Label>Username</Form.Label>
-          <Form.Control autofocus name = "username"/>
+          <Form.Control autoFocus name = "username"/>
         </Form.Group>
        
         <Form.Group controlId = "password" size = "lg">
