457cd5be43c4f7ffbb0ea46717da43a3e3fb8f6e
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-27 11:53:30 +0200

    Return Optional<User> in findByUsername method

--- src/main/java/net/chrzastek/UserRepository.java
+++ src/main/java/net/chrzastek/UserRepository.java
@@ -2,8 +2,8 @@ package net.chrzastek;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  List<User> findByUsername(String username);
+  Optional<User> findByUsername(String username);
 }
