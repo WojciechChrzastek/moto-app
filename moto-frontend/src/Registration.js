1339bee00a3953b77701c8e738baf89f167590f8
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-27 15:30:23 +0200

    Correct imports and typo errors

--- moto-frontend/src/Registration.js
+++ moto-frontend/src/Registration.js
@@ -3,12 +3,15 @@ import './registration.css';
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+// import { Alert } from 'react-bootstrap';
+
+import RegistrationAlert from './RegistrationAlert.js';
 
 class Registration extends Component {
 
   constructor(props) {
     super(props);
-    this.registrationAler = React.createRef();
+    this.RegistrationAlert = React.createRef();
   }
 
   handleSubmit = event => {
@@ -38,22 +41,35 @@ class Registration extends Component {
     });
   }
 
-  render() {
-    return <div className = "Register">
-      <Form onSubmit = {this.handleSubmit}>
-        <Form.Group controlId = "username" size = "lg">
-          <Form.Label>Username</Form.Label>
-          <Form.Control autoFocus name = "username"/>
-        </Form.Group>
-       
-        <Form.Group controlId = "password" size = "lg">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type = "password" name = "password"/>
-        </Form.Group>
+  showRegistrationAlert(variant, heading, message) {
+    this.registrationAlert.current.setVariant(variant);
+    this.registrationAlert.current.setHeading(heading);
+    this.registrationAlert.current.setMessage(message);
+    this.registrationAlert.current.setVisible(true);
+}
 
-        <Button block size = "lg" type = "submit">Register</Button>
-      </Form>
-    </div>
+  render() {
+    return (
+      <>
+        <div className = "Register">
+          <Form onSubmit = {this.handleSubmit}>
+            <Form.Group controlId = "username" size = "lg">
+              <Form.Label>Username</Form.Label>
+              <Form.Control autoFocus name = "username"/>
+            </Form.Group>
+          
+            <Form.Group controlId = "password" size = "lg">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type = "password" name = "password"/>
+            </Form.Group>
+
+            <Button block size = "lg" type = "submit">Register</Button>
+          </Form>
+        </div>
+
+        <RegistrationAlert ref = { this.RegistrationAlert }/>
+      </>
+    )
   }
 }
 
