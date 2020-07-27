c6464e6b8469879b753627b755af2c19e62cc940
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-27 14:19:40 +0200

    Add registration form

--- moto-frontend/src/Registration.js
+++ moto-frontend/src/Registration.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 class Registration extends Component {
   render() {
-    return "Register"
+    return <div className = "Register">
+      <Form>
+        <Form.Group controlId = "username" size = "lg">
+          <Form.Label>Username</Form.Label>
+          <Form.Control autofocus name = "username"/>
+        </Form.Group>
+       
+        <Form.Group controlId = "password" size = "lg">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type = "password" name = "password"/>
+        </Form.Group>
+
+        <Button block size = "lg" type = "submit">Register</Button>
+      </Form>
+    </div>
   }
 }
 
