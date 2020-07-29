2188595fbc6fa6d8aab9fcddb7c1a361480585e5
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-29 15:49:24 +0200

    Add method returning all cars in table

--- src/main/java/net/chrzastek/cars/CarService.java
+++ src/main/java/net/chrzastek/cars/CarService.java
@@ -1,16 +1,20 @@
 package net.chrzastek.cars;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.chrzastek.users.User;
 import net.chrzastek.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,6 +25,15 @@ public class CarService {
 
   @Autowired
   UserRepository userRepository;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @GetMapping("/cars")
+  public ResponseEntity getCars() throws JsonProcessingException {
+    List<Car> cars = carRepository.findAll();
+    return ResponseEntity.ok(objectMapper.writeValueAsString(cars));
+  }
 
   @PostMapping("/cars")
   public ResponseEntity addCar(
