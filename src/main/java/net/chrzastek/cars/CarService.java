8160bce0cd9c42d155e5cd3cd5efde119974d976
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-29 18:45:09 +0200

    Add getCarById method

--- src/main/java/net/chrzastek/cars/CarService.java
+++ src/main/java/net/chrzastek/cars/CarService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,6 +34,15 @@ public class CarService {
   public ResponseEntity getCars() throws JsonProcessingException {
     List<Car> cars = carRepository.findAll();
     return ResponseEntity.ok(objectMapper.writeValueAsString(cars));
+  }
+
+  @GetMapping("/cars/{id}")
+  public ResponseEntity getCarById(@PathVariable long id) {
+    Optional<Car> car = carRepository.findById(id);;
+    if (car.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+    return ResponseEntity.ok(car);
   }
 
   @PostMapping("/cars")
