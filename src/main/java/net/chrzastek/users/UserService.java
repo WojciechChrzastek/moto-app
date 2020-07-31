222751509ee9f4a7b54daea2ab5562acf101513b
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-31 20:25:53 +0200

    Add updateUserById method

--- src/main/java/net/chrzastek/users/UserService.java
+++ src/main/java/net/chrzastek/users/UserService.java
@@ -2,6 +2,7 @@ package net.chrzastek.users;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.chrzastek.cars.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -52,7 +55,26 @@ public class UserService {
     return ResponseEntity.ok(savedUser);
   }
 
+  @PutMapping("/users/{id}")
+  public ResponseEntity updateUserById(
+          @RequestHeader("username") String username,
+          @RequestBody ObjectNode objectNode,
+          @PathVariable long id) {
+    Optional<User> user = userRepository.findById(id);
 
+    if (user.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+
+    User u = userRepository.getOne(id);
+
+    u.setUsername(objectNode.get("username").asText());
+    u.setPassword(objectNode.get("password").asText());
+    u.setEmail(objectNode.get("email").asText());
+
+    userRepository.save(u);
+    return ResponseEntity.ok(u);
+  }
 
   @PostMapping("/login")
   public ResponseEntity login(@RequestBody User user) {
