38f8a01e6af6dad0bd0a8f8e9b01abbfe81fdf59
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-03 15:29:56 +0200

    Use common css file for addCar.js, login,js, register,js, registerAndLoginAlert.js

--- moto-frontend/src/user-login/login.js
+++ moto-frontend/src/user-login/login.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-import '../styles/login.css';
+import '../styles/style.css';
 
 import Alert from '../alert/registerAndLoginAlert.js';
 
