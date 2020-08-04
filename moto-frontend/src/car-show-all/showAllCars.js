865d660b39b1da552fb5272f785224710838289b
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-04 23:40:18 +0200

    Add showAllCars component and its logic

--- moto-frontend/src/car-show-all/showAllCars.js
+++ moto-frontend/src/car-show-all/showAllCars.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
 
 import '../styles/style.css';
+import Table from "react-bootstrap/Table";
 
 class ShowAllCars extends Component {
-    constructor(props) {
-        super(props);
-        this.alert = React.createRef();
+
+    state = {
+        data: [],
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/cars')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({data})
+            });
     }
 
     render() {
-
+        return (
+            <div>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Year</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.data.map(function (car, key) {
+                        return (
+                            <tr key={key}>
+                                <td>{car.id}</td>
+                                <td>{car.brandname}</td>
+                                <td>{car.modelname}</td>
+                                <td>{car.manufactureyear}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </Table>
+            </div>
+        )
     }
-
 }
 
 export default ShowAllCars;
