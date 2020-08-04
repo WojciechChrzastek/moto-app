8a621442986cdb7f61e6c7431561f3fd85e1c3cd
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-04 18:57:10 +0200

    Add bare showAllCars component

---
+++ moto-frontend/src/car-show-all/showAllCars.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import '../styles/style.css';
+
+class ShowAllCars extends Component {
+    constructor(props) {
+        super(props);
+        this.alert = React.createRef();
+    }
+
+    render() {
+
+    }
+
+}
+
+export default ShowAllCars;
