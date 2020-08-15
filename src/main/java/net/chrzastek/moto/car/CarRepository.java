df0ace8c519287c7920934ad29ba1a2926d7479c
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-15 17:12:48 +0200

    Add findByBrandname and findById methods

--- src/main/java/net/chrzastek/moto/car/CarRepository.java
+++ src/main/java/net/chrzastek/moto/car/CarRepository.java
@@ -2,5 +2,9 @@ package net.chrzastek.moto.car;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+  List<Car> findByBrandname(String brandname);
+  List<Car> findById(String id);
 }
