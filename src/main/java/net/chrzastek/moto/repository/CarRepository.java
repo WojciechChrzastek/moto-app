c3ab3de50b5198c45e5d75f1db877548545eca2d
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-26 09:44:50 +0200

    Add more findBy... methods

--- src/main/java/net/chrzastek/moto/repository/CarRepository.java
+++ src/main/java/net/chrzastek/moto/repository/CarRepository.java
@@ -4,8 +4,13 @@ import net.chrzastek.moto.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
+  Optional<Car> findById(long id);
   List<Car> findByBrandnameContaining(String brandname);
-  List<Car> findById(String id);
+  List<Car> findByModelnameContaining(String modelname);
+  List<Car> findByBrandname(String brandname);
+  List<Car> findByModelname(String modelname);
+
 }
