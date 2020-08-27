00bc79849fc680a736515dafc70e8366837360dc
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-27 10:12:45 +0200

    Add findBy... methods

--- src/main/java/net/chrzastek/moto/repository/UserRepository.java
+++ src/main/java/net/chrzastek/moto/repository/UserRepository.java
@@ -3,9 +3,15 @@ package net.chrzastek.moto.repository;
 import net.chrzastek.moto.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
-  Optional<User> findByEmail(String email);
+  Optional<User> findById(long id);
+  List<User> findByUsername(String username);
+  List<User> findByEmail(String email);
+  List<User> findByPassword(String email);
+  List<User> findByUsernameContaining(String username);
+  List<User> findByEmailContaining(String email);
+  List<User> findByPasswordContaining(String password);
 }
