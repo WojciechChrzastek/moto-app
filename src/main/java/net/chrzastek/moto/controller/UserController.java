38d8e29d0d85044905e6fb239d8b2ff2bf789d47
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-08-27 10:14:45 +0200

    Fix addUser, getUsers, updateUserById methods

--- src/main/java/net/chrzastek/moto/controller/UserController.java
+++ src/main/java/net/chrzastek/moto/controller/UserController.java
@@ -1,9 +1,8 @@
 package net.chrzastek.moto.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.chrzastek.moto.AllowedCors;
+import net.chrzastek.moto.entity.Car;
 import net.chrzastek.moto.entity.User;
 import net.chrzastek.moto.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,22 +35,54 @@ public class UserController {
   }
 
   @PostMapping("/users")
-  public ResponseEntity addUser(@RequestBody User user) {
+  public ResponseEntity<User> addUser(@RequestBody User user) {
+    Optional<User> userOptional = userRepository.findById(user.getId());
+
     if (user.getUsername().equals("") || user.getPassword().equals("") || user.getEmail().equals("")) {
       return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
-    } else if (userRepository.findByUsername(user.getUsername()).isPresent()
-            || userRepository.findByEmail(user.getEmail()).isPresent()) {
+    } else if (userOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    } else if (!userRepository.findByUsername(user.getUsername()).isEmpty()
+            || !userRepository.findByEmail(user.getEmail()).isEmpty()) {
       return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+
     } else {
-      User savedUser = userRepository.save(user);
-      return ResponseEntity.ok(savedUser);
+      userRepository.save(user);
+      return ResponseEntity.ok(user);
     }
+
   }
 
   @GetMapping("/users")
-  public ResponseEntity getUsers() throws JsonProcessingException {
-    List<User> users = userRepository.findAll();
-    return ResponseEntity.ok(objectMapper.writeValueAsString(users));
+  public ResponseEntity<List<User>> getUsers(
+          @RequestParam(required = false) String username,
+          @RequestParam(required = false) String email,
+          @RequestParam(required = false) String password
+  ) {
+
+    try {
+      List<User> users = new ArrayList<>();
+
+      if (username == null && email == null && password == null) {
+        users.addAll(userRepository.findAll());
+      } else if (username == null && email == null) {
+        users.addAll(userRepository.findByPasswordContaining(password));
+      } else if (username == null && password == null) {
+        users.addAll(userRepository.findByEmailContaining(email));
+      } else {
+        users.addAll(userRepository.findByUsernameContaining(username));
+      }
+
+      if (users.isEmpty()) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+
+      return new ResponseEntity<>(users, HttpStatus.OK);
+    } catch (
+            Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @GetMapping("/users/{id}")
@@ -63,45 +95,44 @@ public class UserController {
     return ResponseEntity.ok(user);
   }
 
-  @GetMapping("/users/name")
-  public ResponseEntity getUserByUsername(@RequestBody ObjectNode objectNode) {
-    Optional<User> user = userRepository.findByUsername(objectNode.get("username").asText());
+//  @GetMapping("/users/name")
+//  public ResponseEntity getUserByUsername(@RequestBody ObjectNode objectNode) {
+//    Optional<User> user = userRepository.findByUsername(objectNode.get("username").asText());
+//
+//    if (user.isEmpty()) {
+//      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+//    }
+//    return ResponseEntity.ok(user);
+//  }
 
-    if (user.isEmpty()) {
-      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
-    }
-    return ResponseEntity.ok(user);
-  }
-
-  @GetMapping("/users/email")
-  public ResponseEntity getUserByEmail(@RequestBody ObjectNode objectNode) {
-    Optional<User> user = userRepository.findByEmail(objectNode.get("email").asText());
-
-    if (user.isEmpty()) {
-      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
-    }
-    return ResponseEntity.ok(user);
-  }
+//  @GetMapping("/users/email")
+//  public ResponseEntity getUserByEmail(@RequestBody ObjectNode objectNode) {
+//    Optional<User> user = userRepository.findByEmail(objectNode.get("email").asText());
+//
+//    if (user.isEmpty()) {
+//      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+//    }
+//    return ResponseEntity.ok(user);
+//  }
 
   @PutMapping("/users/{id}")
   public ResponseEntity updateUserById(
-          @RequestHeader("username") String username,
-          @RequestBody ObjectNode objectNode,
+//          @RequestHeader("username") String username,
+          @RequestBody User user,
           @PathVariable long id) {
-    Optional<User> user = userRepository.findById(id);
+    Optional<User> userr = userRepository.findById(id);
 
-    if (user.isEmpty()) {
-      return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    if (userr.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    User u = userRepository.getOne(id);
+//    User u = userRepository.getOne(id);
 
-    u.setUsername(objectNode.get("username").asText());
-    u.setPassword(objectNode.get("password").asText());
-    u.setEmail(objectNode.get("email").asText());
+//    u.setUsername(objectNode.get("username").asText());
+//    u.setPassword(objectNode.get("password").asText());
+//    u.setEmail(objectNode.get("email").asText());
 
-    userRepository.save(u);
-    return ResponseEntity.ok(u);
+    return validateResponse(user);
   }
 
   @DeleteMapping("/users/{id}")
@@ -119,19 +150,28 @@ public class UserController {
     return ResponseEntity.ok(u);
   }
 
-  @PostMapping("/login")
-  public ResponseEntity login(@RequestBody User user) {
-    Optional<User> userFromDb = userRepository.findByUsername(user.getUsername());
-
-    if (userFromDb.isEmpty() || hasWrongPassword(userFromDb, user)) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-    return ResponseEntity.ok().build();
-
-  }
+//  @PostMapping("/login")
+//  public ResponseEntity login(@RequestBody User user) {
+//    Optional<User> userFromDb = userRepository.findByUsername(user.getUsername());
+//
+//    if (userFromDb.isEmpty() || hasWrongPassword(userFromDb, user)) {
+//      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//    }
+//    return ResponseEntity.ok().build();
+//
+//  }
 
   private boolean hasWrongPassword(Optional<User> userFromDb, User user) {
     return !userFromDb.get().getPassword().equals(user.getPassword());
+  }
+
+  private ResponseEntity<User> validateResponse(@RequestBody User user) {
+    if (user.getUsername().equals("") || user.getEmail().equals("") || user.getPassword().equals("")) {
+      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+    } else {
+      userRepository.save(user);
+      return ResponseEntity.ok(user);
+    }
   }
 
 }
